65bb5e39f6df4a82407de1f56b63c3a8456fcd47
Wepa <wepa86802@example.com>
2025-12-18 18:15:08 +0500

    Add IP-based access control middleware added

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\BeletController;
-use App\Http\Controllers\Api\BeletCheckPhoneController;
-use App\Http\Controllers\Api\OrderStatusController;
 use Illuminate\Support\Facades\Route;
 
-//Route::post('/check-phone', [BeletCheckPhoneController::class, 'checkPhone']);
-//Route::get('/banks', [BeletController::class, 'banks']);
-//Route::get('/balance/recommendations', [BeletBalanceController::class, 'balances']);
-//
-//Route::post('/balance/top-up', [BeletBalanceController::class, 'topUp']);
-//Route::post('/balance/confirm', [BeletBalanceController::class, 'confirm']);
-//Route::get('/orders/{id}/status', [OrderStatusController::class, 'status']);
+
 Route::prefix('belet')->group(function () {
     Route::get('banks', [BeletController::class, 'banks']);
     Route::get('balances', [BeletController::class, 'balances']);
